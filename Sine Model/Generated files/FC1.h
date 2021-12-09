16bc3084efd0db6298a358e2c4e4d5ce8ee2c5f0
Sailesh M <43719932+Saileshmurali@users.noreply.github.com>
2021-12-09 14:25:17 +0530

    File generated automatically

---
+++ Sine Model/Generated files/FC1.h
@@ -0,0 +1,10 @@
+#include  <arm_const_structs.h>
+#include  <arm_nnfunctions.h>
+#define	IP1_IN_DIM	1
+#define	IP1_OUT_DIM	16
+#define	IP1_BIAS_LSHIFT	4
+#define	IP1_OUT_RSHIFT	6
+const q7_t wf_1[IP1_OUT_DIM*IP1_IN_DIM]={
+-54,31,23,-3,-26,37,-49,58,0,0,3,-65,-63,35,-33,-26};
+const q7_t bf_1[IP1_OUT_DIM]={
+0,-72,-31,11,0,0,0,-29,0,0,112,0,0,0,0,0};
